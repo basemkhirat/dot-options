bd747f5c4d731c935ecd79a563a0232f242c6110
Mohamed Farag <mohamedfarag@dotdev.ae>
2017-09-19 15:16:20 +0200

    Initial import of module

---
+++ src/Controllers/SitemapController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dot\Options\Controllers;
+
+use Dot\Controller;
+
+class SitemapController extends Controller {
+
+    public function update() {
+        //Sitemap::render("");
+        //return date(Config("date_format"), time());
+    }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * WEB
+ */
+Route::group(array(
+    "prefix" => ADMIN,
+    "middleware" => ["web", "auth"],
+        ), function($route) {
+    $route->group(array("prefix" => "options"), function($route) {
+        $route->any('/', array("as" => "admin.options.show", "uses" => "Dot\Options\Controllers\OptionsController@index"));
+        $route->any('/seo', array("as" => "admin.options.seo", "uses" => "Dot\Options\Controllers\OptionsController@seo"));
+        $route->any('/modules', array("as" => "admin.options.modules", "uses" => "Dot\Options\Controllers\OptionsController@modules"));
+        $route->any('/media', array("as" => "admin.options.media", "uses" => "Dot\Options\Controllers\OptionsController@media"));
+        $route->any('/social', array("as" => "admin.options.social", "uses" => "Dot\Options\Controllers\OptionsController@social"));
+        $route->any('/plugins', array("as" => "admin.options.plugins", "uses" => "Dot\Options\Controllers\OptionsController@plugins"));
+        $route->any('/check_update', array("as" => "admin.options.check_update", "uses" => "Dot\Options\Controllers\OptionsController@check_update"));
+        $route->any('/plugins/activation/{name}/{status}/{step?}', array("as" => "admin.plugins.activation", "uses" => "Dot\Options\Controllers\OptionsController@plugin"));
+    });
+});
+
+Route::any('sitemap', array("as" => "admin.sitemap.update", "uses" => 'SitemapController@update'));
+
+/*
+ * API
+ */
+Route::group([
+    "prefix" => API,
+    "middleware" => ["auth:api"]
+], function ($route) {
+    $route->get("/options/show", "Dot\Options\Controllers\OptionsApiController@show");
+    $route->post("/options/create", "Dot\Options\Controllers\OptionsApiController@create");
+    $route->post("/options/update", "Dot\Options\Controllers\OptionsApiController@update");
+    $route->post("/options/destroy", "Dot\Options\Controllers\OptionsApiController@destroy");
+});
+
+
